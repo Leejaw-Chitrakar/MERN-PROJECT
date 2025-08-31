746872669b6c583773ad1d6fbcd19a8805039dbe
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-08-31 16:17:38 +0545

    first commit

---
+++ .history/src/App_20250831151626.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import React from 'react';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      {/* <Home /> */}
+    <RouterProvider router={router} />
+
+    </div>
+  );
+}
+
+export default App;
